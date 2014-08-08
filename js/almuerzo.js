9ef7f7076b1246f620359bfc4b4da669c76b882e
Daniel F Moisset <dmoisset@machinalis.com>
2014-08-08 12:05:47 -0300

    Manejo de comensales

--- js/almuerzo.js
+++ js/almuerzo.js
@@ -24,4 +24,44 @@ function Almuerzo($scope) {
         $scope.comidas.splice(index, 1);
     }
 
+    $scope.comensales = [
+        {"name": "Elías", "paid": 100, "comidas": {}},
+        {"name": "Julián", "paid": 50, "comidas": {}},
+        {"name": "idacal", "paid": 0, "comidas": {}},
+    ];
+
+    $scope.precioPorPera = function(index) {
+        var comida = $scope.comidas[index];
+        var nComensales = 0;
+        for (var i = 0; i < $scope.comensales.length; i++ ) {
+            if ($scope.comensales[i].comidas[comida.name])
+                nComensales++;
+        }
+        return comida.cost / nComensales;
+    }
+
+    $scope.aPagar = function(index) {
+        var comensal = $scope.comensales[index];
+        var total = 0;
+        for (var i = 0; i < $scope.comidas.length; i++ ) {
+            if (comensal.comidas[$scope.comidas[i].name])
+                total += $scope.precioPorPera(i);
+        }
+        return total;
+    }
+
+    $scope.masComensales = function () {
+        $scope.comensales.push({
+            "name": "Juan",
+            "paid": 0,
+            "comidas": {}
+        });
+    }
+
+    $scope.menosComensales = function (index) {
+        $scope.comensales.splice(index, 1);
+    }
+
+
 }
+
